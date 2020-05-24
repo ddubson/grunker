e2fdda0c0dfaf37b30defa32ce0f53fc1b32bfc9
Dmitriy Dubson <d.dubson@gmail.com>
2020-05-24 18:25:08 -0400

    Formatted open data with bootstrap css

--- grunker-web-ts/src/App.tsx
+++ grunker-web-ts/src/App.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Navbar, NavbarBrand} from "react-bootstrap";
+import {Navbar, NavbarBrand, Table} from "react-bootstrap";
 import axios from "axios";
 
 interface Nyc311DataItem {
@@ -19,14 +19,36 @@ function App() {
       .then(items => setItems(items))
   }, [])
 
+  const renderItem = (item: Nyc311DataItem) => (
+    <tr key={item.unique_key}>
+      <td>{item.agency}</td>
+      <td>{item.city}</td>
+      <td>{item.descriptor}</td>
+      <td>{item.status}</td>
+    </tr>
+  );
+
   return (
     <>
-      <Navbar bg="dark">
+      <Navbar bg="dark" variant="dark">
         <NavbarBrand>
+          Grunker
         </NavbarBrand>
       </Navbar>
       <section>
-        {items.map((item: Nyc311DataItem) => <div key={item.unique_key}>[{item.agency}] {item.descriptor}</div>)}
+        <Table>
+          <thead>
+          <tr>
+            <th>Agency</th>
+            <th>City</th>
+            <th>Descriptor</th>
+            <th>Status</th>
+          </tr>
+          </thead>
+          <tbody>
+          {items.map(renderItem)}
+          </tbody>
+        </Table>
       </section>
     </>
   );
